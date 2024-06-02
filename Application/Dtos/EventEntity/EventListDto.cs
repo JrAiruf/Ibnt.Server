a5820ceec68a51c383d6eb1d7a74dd81bca47a4b
Jr_Silva_Dev <jos.pd2019@gmail.com>
2024-06-02 15:55:00 -0300

    :hammer: FIX: Adding Entity Type Property To EventDto

--- Application/Dtos/EventEntity/EventListDto.cs
+++ Application/Dtos/EventEntity/EventListDto.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; init; }
         public string Title { get; init; }
         public string? ImageUrl { get; init; }
+        public string? EntityType{ get; init; }
         public DateTime? PostDate { get; init; }
         public DateTime? Date { get; init; }
         public string Description { get; init; }
