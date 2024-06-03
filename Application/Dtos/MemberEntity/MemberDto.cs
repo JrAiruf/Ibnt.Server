7a222e46bf5abc2a2bb18e5c3b7177c29d9c020c
Jr_Silva_Dev <jos.pd2019@gmail.com>
2024-06-03 07:05:56 -0300

    :hammer: FIX:Fixing Response from Members By Id endpoint

--- Application/Dtos/MemberEntity/MemberDto.cs
+++ Application/Dtos/MemberEntity/MemberDto.cs
@@ -8,6 +8,6 @@ namespace Ibnt.Server.Application.Dtos.MemberEntity
         public Guid Id { get; init; }
         public string FullName { get; init; }
         public string? ProfileImage { get; init; }
-        public AuthResponseDto Credential { get; init; }
+        public MemberCredentialDto? Credential { get; init; }
     }
 }
